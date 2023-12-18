db5921505e31d6e7c6dc7975fdcea699ae49491e
Lizz1102 <lizz05110@gmail.com>
2023-12-18 12:12:11 -0500

    Create 404 page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
         <NavBar />
         <div id="page-body">
           <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/about' element={<AboutPage />} />
-            <Route path='/articles' element={<ArticlesListPage />} />
-            <Route path='/articles/:articleId' element={<ArticlePage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
       </div>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,17 +1,20 @@
 import { useParams } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
 import articles from "./article-content";
 
 const ArticlePage = () => {
   const { articleId } = useParams();
-
   const article = articles.find((article) => article.name === articleId);
+
+  if (!article) { 
+    return <NotFoundPage />
+  }
 
   return (
     <>
       <h1>{article.title}</h1>
       {article.content.map((paragraph, index) => (
-        //Note: don't use index as key like this if paragraphs may change
-        <p key={index}>{paragraph}</p>
+        <p key={index}>{paragraph}</p> //Note: don't use index as key like this if paragraphs may change
       ))}
     </>
   );
