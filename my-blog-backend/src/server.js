1e09e9969a4121a0b1b9bdf755b12a5cbf847a89
Lizz1102 <lizz05110@gmail.com>
2023-12-19 12:25:33 -0500

    Add feature: comments

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -4,14 +4,17 @@ let articlesInfo = [
   {
     name: "spirit-animal",
     upvotes: 0,
+    comments: [],
   },
   {
     name: "liza-startup-ideas",
     upvotes: 0,
+    comments: [],
   },
   {
     name: "the-art-of-joy",
     upvotes: 0,
+    comments: [],
   },
 ];
 
@@ -23,9 +26,22 @@ app.put("/api/articles/:name/upvote", (req, res) => {
   const article = articlesInfo.find((a) => a.name === name);
   if (article) {
     article.upvotes += 1;
-    res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
   } else {
     res.send(`Article doesn\'t exist!`);
+  }
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articlesInfo.find((a) => a.name === name);
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
+  } else {
+    res.send(`Article doesn\'t existXXXXX!`);
   }
 });
 
