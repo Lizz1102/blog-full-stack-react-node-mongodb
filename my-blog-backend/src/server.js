16679dc4d0a40cabca0beaf21481b8a8b4f0f223
Lizz1102 <lizz05110@gmail.com>
2023-12-19 11:51:38 -0500

    Implement api for upvoting articles

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,11 +1,34 @@
 import express from "express";
 
-const app = express();
+let articlesInfo = [
+  {
+    name: "spirit-animal",
+    upvotes: 0,
+  },
+  {
+    name: "liza-startup-ideas",
+    upvotes: 0,
+  },
+  {
+    name: "the-art-of-joy",
+    upvotes: 0,
+  },
+];
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+const app = express();
+app.use(express.json());
+
+app.put("/api/articles/:name/upvote", (req, res) => {
+  const { name } = req.params;
+  const article = articlesInfo.find((a) => a.name === name);
+  if (article) {
+    article.upvotes += 1;
+    res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+  } else {
+    res.send(`Article doesn\'t exist!`);
+  }
 });
 
 app.listen(8000, () => {
-    console.log('Server is listening on port 8000');
+  console.log("Server is listening on port 8000");
 });
