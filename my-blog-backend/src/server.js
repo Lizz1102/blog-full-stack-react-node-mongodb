9719574d765294f5dde90d9a97586a5e1329f628
Lizz1102 <lizz05110@gmail.com>
2023-12-20 15:21:50 -0500

    Replace in-memory db with mongodb connection

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,25 +1,24 @@
 import express from "express";
-
-let articlesInfo = [
-  {
-    name: "spirit-animal",
-    upvotes: 0,
-    comments: [],
-  },
-  {
-    name: "liza-startup-ideas",
-    upvotes: 0,
-    comments: [],
-  },
-  {
-    name: "the-art-of-joy",
-    upvotes: 0,
-    comments: [],
-  },
-];
+import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(express.json());
+
+app.get("/api/articles/:name", async (req, res) => {
+  const { name } = req.params;
+
+  const client = new MongoClient('mongodb://127.0.0.1:27017');
+  await client.connect();
+
+  const db = client.db('react-blog-db');
+  const article = await db.collection('articles').findOne({ name });
+
+  if (article) {
+    res.json(article);
+  } else { 
+    res.sendStatus(404);
+  }
+});
 
 app.put("/api/articles/:name/upvote", (req, res) => {
   const { name } = req.params;
@@ -41,7 +40,7 @@ app.post("/api/articles/:name/comments", (req, res) => {
     article.comments.push({ postedBy, text });
     res.send(article.comments);
   } else {
-    res.send(`Article doesn\'t existXXXXX!`);
+    res.send(`Article doesn\'t exist!`);
   }
 });
 
