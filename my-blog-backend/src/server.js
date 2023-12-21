5ba410852729ddcc2403cf97a7ad6789fab4c462
Lizz1102 <lizz05110@gmail.com>
2023-12-21 11:11:07 -0500

    Update comments endpoint: use MongoDB to store comments

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -11,8 +11,8 @@ app.get("/api/articles/:name", async (req, res) => {
   await client.connect();
 
   const db = client.db("react-blog-db");
-  const article = await db.collection("articles").findOne({ name });
 
+  const article = await db.collection("articles").findOne({ name });
   if (article) {
     res.json(article);
   } else {
@@ -33,8 +33,8 @@ app.put("/api/articles/:name/upvote", async (req, res) => {
       $inc: { upvotes: 1 },
     }
   );
+
   const article = await db.collection("articles").findOne({ name });
-  
   if (article) {
     article.upvotes += 1;
     res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
@@ -43,13 +43,23 @@ app.put("/api/articles/:name/upvote", async (req, res) => {
   }
 });
 
-app.post("/api/articles/:name/comments", (req, res) => {
+app.post("/api/articles/:name/comments", async (req, res) => {
   const { name } = req.params;
   const { postedBy, text } = req.body;
 
-  const article = articlesInfo.find((a) => a.name === name);
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+
+  const db = client.db("react-blog-db");
+  await db.collection("articles").updateOne(
+    { name },
+    {
+      $push: { comments: { postedBy, text } },
+    }
+  );
+
+  const article = await db.collection("articles").findOne({ name });
   if (article) {
-    article.comments.push({ postedBy, text });
     res.send(article.comments);
   } else {
     res.send(`Article doesn\'t exist!`);
