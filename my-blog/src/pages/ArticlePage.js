c074317dbd0abae88f4fcd2504bee1c8e16e7039
Lizz1102 <lizz05110@gmail.com>
2023-12-21 14:03:46 -0500

    Load article info through axios api call

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,23 +1,43 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import NotFoundPage from "./NotFoundPage";
 import articles from "./article-content";
 
 const ArticlePage = () => {
-  const { articleId } = useParams();
-  const article = articles.find((article) => article.name === articleId);
+    const [articleInfo, setArticleInfo] = useState({
+        upvotes: 0,
+        comments: [],
+    });
+    const { articleId } = useParams();
 
-  if (!article) { 
-    return <NotFoundPage />
-  }
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(
+                `/api/articles/${articleId}`
+            );
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        };
 
-  return (
-    <>
-      <h1>{article.title}</h1>
-      {article.content.map((paragraph, index) => (
-        <p key={index}>{paragraph}</p> //Note: don't use index as key like this if paragraphs may change
-      ))}
-    </>
-  );
+        loadArticleInfo();
+    }, []);
+
+    const article = articles.find((article) => article.name === articleId);
+
+    if (!article) {
+        return <NotFoundPage />;
+    }
+
+    return (
+        <>
+            <h1>{article.title}</h1>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+            {article.content.map((paragraph, index) => (
+                <p key={index}>{paragraph}</p> //Note: don't use index as key like this if paragraphs may change
+            ))}
+        </>
+    );
 };
 
 export default ArticlePage;
