1e8af122b6d71d7473a291431254eb1f7da226a5
Lizz1102 <lizz05110@gmail.com>
2023-12-21 11:31:09 -0500

    Separate database connection codes

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,16 +1,11 @@
 import express from "express";
-import { MongoClient } from "mongodb";
+import { db, connectToDb } from "./db.js";
 
 const app = express();
 app.use(express.json());
 
 app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
-
-  const client = new MongoClient("mongodb://127.0.0.1:27017");
-  await client.connect();
-
-  const db = client.db("react-blog-db");
 
   const article = await db.collection("articles").findOne({ name });
   if (article) {
@@ -23,10 +18,6 @@ app.get("/api/articles/:name", async (req, res) => {
 app.put("/api/articles/:name/upvote", async (req, res) => {
   const { name } = req.params;
 
-  const client = new MongoClient("mongodb://127.0.0.1:27017");
-  await client.connect();
-
-  const db = client.db("react-blog-db");
   await db.collection("articles").updateOne(
     { name },
     {
@@ -36,7 +27,6 @@ app.put("/api/articles/:name/upvote", async (req, res) => {
 
   const article = await db.collection("articles").findOne({ name });
   if (article) {
-    article.upvotes += 1;
     res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
   } else {
     res.send(`Article doesn\'t exist!`);
@@ -47,10 +37,6 @@ app.post("/api/articles/:name/comments", async (req, res) => {
   const { name } = req.params;
   const { postedBy, text } = req.body;
 
-  const client = new MongoClient("mongodb://127.0.0.1:27017");
-  await client.connect();
-
-  const db = client.db("react-blog-db");
   await db.collection("articles").updateOne(
     { name },
     {
@@ -66,6 +52,9 @@ app.post("/api/articles/:name/comments", async (req, res) => {
   }
 });
 
-app.listen(8000, () => {
-  console.log("Server is listening on port 8000");
+connectToDb(() => {
+  console.log("Successfully connected to database!");
+  app.listen(8000, () => {
+    console.log("Server is listening on port 8000");
+  });
 });
