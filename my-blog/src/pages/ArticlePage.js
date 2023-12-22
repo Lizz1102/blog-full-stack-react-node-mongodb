a996d8366c3bab01a3d9eac1c20fd89c8e243613
Lizz1102 <lizz05110@gmail.com>
2023-12-22 14:33:49 -0500

    Create custom auth hook for login

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import NotFoundPage from "./NotFoundPage";
 import CommentsList from "../components/CommentsList";
 import AddCommentForm from "../components/AddCommentForm";
+import useUser from "../hooks/useUser";
 import articles from "./article-content";
 
 const ArticlePage = () => {
@@ -12,6 +13,8 @@ const ArticlePage = () => {
         comments: [],
     });
     const { articleId } = useParams();
+
+    const { user, isLoading } = useUser();
 
     useEffect(() => {
         const loadArticleInfo = async () => {
@@ -39,18 +42,26 @@ const ArticlePage = () => {
         <>
             <h1>{article.title}</h1>
             <div className="upvotes-section">
-                <button onClick={addUpvote}>Upvote</button>
+                {user ? (
+                    <button onClick={addUpvote}>Upvote</button>
+                ) : (
+                    <button>Log in to upvote</button>
+                )}
                 <p>This article has {articleInfo.upvotes} upvote(s)</p>
             </div>
             {article.content.map((paragraph, index) => (
                 <p key={index}>{paragraph}</p> //Note: don't use index as key like this if paragraphs may change
             ))}
-            <AddCommentForm
-                articleName={articleId}
-                onArticleUpdated={(updatedArticle) =>
-                    setArticleInfo(updatedArticle)
-                }
-            />
+            {user ? (
+                <AddCommentForm
+                    articleName={articleId}
+                    onArticleUpdated={(updatedArticle) =>
+                        setArticleInfo(updatedArticle)
+                    }
+                />
+            ) : (
+                <button>Log in to add a comment</button>
+            )}
             <CommentsList comments={articleInfo.comments} />
         </>
     );
