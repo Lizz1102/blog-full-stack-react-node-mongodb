2b91f3b363b72c2912d528541bc20acc91052920
Lizz1102 <lizz05110@gmail.com>
2023-12-22 11:54:21 -0500

    Add Firebase Auth to react app

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const firebaseConfig = {
+    apiKey: "AIzaSyAD-HwdcUWF_GaLn1j2M_FIth1oK9KswLk",
+    authDomain: "my-react-blog-8cc92.firebaseapp.com",
+    projectId: "my-react-blog-8cc92",
+    storageBucket: "my-react-blog-8cc92.appspot.com",
+    messagingSenderId: "951995222759",
+    appId: "1:951995222759:web:e4c3f87aa15b4a439df042",
+    measurementId: "G-YE85K8J73P",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
